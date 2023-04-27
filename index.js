e71f34c640810ea7fda66ee9e54f78af8aca93b8
leduyphong123 <leduyphongck789@gmail.com>
2023-04-27 15:26:44 +0700

    add imga

--- index.js
+++ index.js
@@ -1,50 +1,79 @@
 let ctx = document.getElementById("canvasGame");
 let cv = ctx.getContext("2d");
 
+//img
+let heroIdle = new Image();
+let heroWalk = new Image();
+let heroJump = new Image();
+let heroAttack = new Image();
+let monsterBatIdle = new Image();
+let monsterSkill = new Image();
+let mapDefault = new Image();
+heroIdle.src = "img/hero/Idle.png";
+heroWalk.src = "img/hero/Walk.png";
+heroJump.src = "img/hero/Jump.png";
+heroAttack.src = "img/hero/Attack.png";
+monsterBatIdle.src = "img/monster/BatAlbino_Flying.png";
+monsterSkill.src = "img/Free_Skills.png";
+mapDefault.src = "img/map/Basic_Top.png";
+
 
 ctx.width = innerWidth;
 ctx.height = innerHeight;
-function Player(positer, velocity) {
+function Player(positer, velocity, img) {
     this.positer = positer;
     this.velocity = velocity;
-    this.width = 30;
+    this.width = 100;
     this.height = 100;
     this.gravitation = 0.2; //trọng lực rơi
-    this.hp=1000;
+    this.hp = 1000;
     this.isAttack;
-    this.offSet={x:0,y:0}
     this.attackBox = {
         positer: {
-            x:this.positer.x,
-            y:this.positer.y
+            x: this.positer.x,
+            y: this.positer.y
         },
-        velocity:{
-            x:0,
-            y:0
+        velocity: {
+            x: 0,
+            y: 0
         },
         width: 70,
         height: this.height,
+    };
+    this.img = img;
+    this.imgIndex = 0;
+    this.imgIndexEnd = 14;
+    this.imgAnimation = {
+        idle: { imge: heroIdle, indexNumber: 14, isThis: true },
+        walk: { imge: heroWalk, indexNumber: 8 },
+        jump: { imge: heroJump, indexNumber: 19 },
+        attack: { imge: heroAttack, indexNumber: 7, isThis: false },
     }
-
+    this.imgRun = this.imgAnimation.idle.imge;
     this.draw = function () {
-        if(this.hp>0){
-            cv.fillStyle = "red";
-            cv.fillRect(this.positer.x, this.positer.y, this.width, this.height);
-    
+        if (this.hp > 0) {
+            cv.drawImage(this.imgRun, 64 * this.imgIndex, 0, 64, 64, this.positer.x, this.positer.y, this.width, this.height);
+
             //attackbox
             if (this.isAttack) {
-                cv.fillStyle = "black";
-                cv.fillRect(this.attackBox.positer.x, this.attackBox.positer.y, this.attackBox.width, this.attackBox.height);
+                // cv.fillStyle = "black";
+                // cv.fillRect(this.attackBox.positer.x, this.attackBox.positer.y, this.attackBox.width, this.attackBox.height);
+                cv.drawImage(this.imgRun, 64 * this.imgIndex, 0, 64, 64, this.attackBox.positer.x, this.attackBox.positer.y, this.attackBox.width, this.attackBox.height);
+
             }
         }
 
     }
     this.update = function () {
+        this.imgIndex++;
+        if (this.imgIndex > this.imgIndexEnd - 1) {
+            this.imgIndex = 0;
+        }
         this.draw();
         this.positer.x += this.velocity.x;
         this.positer.y += this.velocity.y;
-        this.attackBox.positer.x=this.positer.x+this.attackBox.velocity.x;
-        this.attackBox.positer.y=this.positer.y
+        this.attackBox.positer.x = this.positer.x + this.attackBox.velocity.x;
+        this.attackBox.positer.y = this.positer.y
         //check vat the roi toi man hinh duoi chua
         if (this.positer.y + this.height >= ctx.height) {
             this.velocity.y = 0;
@@ -57,6 +86,7 @@ function Player(positer, velocity) {
             setTimeout(() => { this.isAttack = false }, 100);
         }
 
+
     }
 }
 
@@ -64,8 +94,10 @@ function MapCreate(positer, status) {
     this.positer = positer;
     this.status = status;
     this.draw = function () {
-        cv.fillStyle = "blue";
-        cv.fillRect(this.positer.x, this.positer.y, this.status.width, this.status.height);
+        // cv.fillStyle = "blue";
+        // cv.fillRect(this.positer.x, this.positer.y, this.status.width, this.status.height);
+        cv.drawImage(mapDefault, this.positer.x, this.positer.y, this.status.width, this.status.height);
+
     }
 }
 
@@ -88,26 +120,37 @@ function Monster(positer) {
         width: 20,
         height: 20,
     }
-    this.draw = function () {
-        if(this.hp>0){
+    this.img = monsterBatIdle;
+    this.imgIndex = 0;
+    this.imgIndexEnd = 4;
+    this.imgAnimation = {
+        idle: { imge: monsterBatIdle, indexNumber: 4 },
+        skill: { imge: monsterSkill, indexNumber: 1 },
 
-            cv.fillStyle = "yellow";
-            cv.fillRect(this.positer.x, this.positer.y, this.width, this.height);
-    
+    }
+    this.imgRun = this.imgAnimation.idle.imge;
+    this.draw = function () {
+        if (this.hp > 0) {
+
+            cv.drawImage(this.imgRun, 32 * this.imgIndex, 0, 32, 32, this.positer.x, this.positer.y, this.width, this.height);
             //hp
             cv.fillStyle = "gray";
             cv.fillRect(this.positer.x, this.positer.y - 20, this.width, 10);
             cv.fillStyle = "red";
             cv.fillRect(this.positer.x, this.positer.y - 20, this.hp, 10);
-    
+
             //attack
-            if (this.monsterHits) {   
-                cv.fillStyle = "red";
-                cv.fillRect(this.attackBox.positer.x, this.attackBox.positer.y, this.attackBox.width, this.attackBox.height);
+            if (this.monsterHits) {
+            cv.drawImage(this.imgAnimation.skill.imge, 32 * this.imgIndex, 0, 32, 32, this.attackBox.positer.x, this.attackBox.positer.y, this.attackBox.width, this.attackBox.height);
+
             }
         }
     }
     this.update = function () {
+        this.imgIndex++;
+        if(this.imgIndex>this.imgIndexEnd-1){
+            this.imgIndex=0;
+        }
         this.draw();
         this.attackBox.positer.x += this.attackBox.velocity.x;
         this.attackBox.positer.y += this.attackBox.velocity.y;
@@ -115,16 +158,16 @@ function Monster(positer) {
 }
 
 //khoi tao doi tuong
-const player = new Player({ x: 100, y: 430 }, { x: 0, y: 10 });
+const player = new Player({ x: 100, y: 430 }, { x: 0, y: 10 }, heroIdle);
 
-let maps = [new MapCreate({ x: 0, y: ctx.height - 10 }, { width: 2000, height: 10 }),
+let maps = [new MapCreate({ x: 0, y: ctx.height - 22 }, { width: 2000, height: 30 }),
 new MapCreate({ x: 300, y: 250 }, { width: 30, height: 10 }),
 new MapCreate({ x: 400, y: 150 }, { width: 200, height: 10 }),
 new MapCreate({ x: 500, y: 350 }, { width: 200, height: 10 }),
 new MapCreate({ x: 700, y: 350 }, { width: 200, height: 10 }),
 ];
 
-let monster = new Monster({ x: 200, y: ctx.height - 80 });
+let monster = new Monster({ x: 200, y: ctx.height - 70 });
 //key is event
 let keyEvt = {
     right: {
@@ -147,7 +190,7 @@ function startGame() {
     for (let i in maps) {
         maps[i].draw();
         //dung tren map
-        if (player.positer.y + player.height <= maps[i].positer.y
+        if (player.positer.y + player.height+10 <= maps[i].positer.y
             && player.positer.y + player.height + player.velocity.y >= maps[i].positer.y
             && player.positer.x + player.width >= maps[i].positer.x
             && player.positer.x <= maps[i].positer.x + maps[i].status.width) {
@@ -186,7 +229,7 @@ function startGame() {
 
 
     //attack hero
-    if(keyEvt.dame.visit===true){
+    if (keyEvt.dame.visit === true) {
         player.attack();
 
     }
@@ -206,7 +249,7 @@ function startGame() {
     }
 
     // monster dame hero
-    if ( monster.hp > 0 && player.hp>0
+    if (monster.hp > 0 && player.hp > 0
         && player.positer.x >= (monster.positer.x - 200)
         && player.positer.x + player.width <= (monster.positer.x + monster.width + 200)
         && monster.monsterHits == true) {
@@ -215,7 +258,7 @@ function startGame() {
             monster.attackBox.velocity.x = - 1;
             if (monster.attackBox.positer.x <= (player.positer.x + player.width)
             ) {
-                player.hp-=10;
+                player.hp -= 10;
                 console.log(player.hp);
 
                 monster.attackBox.velocity.x = 0;
@@ -225,7 +268,7 @@ function startGame() {
         } else if (player.positer.x >= monster.positer.x + monster.width) {
             monster.attackBox.velocity.x = 1;
             if (monster.attackBox.positer.x + monster.attackBox.width >= player.positer.x) {
-                player.hp-=100;
+                player.hp -= 100;
                 console.log(player.hp);
                 monster.attackBox.velocity.x = 0;
                 monster.attackBox.positer.x = monster.positer.x;
@@ -246,21 +289,33 @@ window.addEventListener("keydown", (evt) => {
     switch (evt.code) {
         case "KeyD":
             keyEvt.right.visit = true;
-            player.attackBox.velocity.x=0;
+            player.attackBox.velocity.x = 0;
+            player.imgRun = player.imgAnimation.walk.imge;
+            player.imgIndexEnd = player.imgAnimation.walk.indexNumber;
 
             break;
         case "KeyA":
             keyEvt.left.visit = true;
-            player.attackBox.velocity.x=-40;
+            player.attackBox.velocity.x = -40;
+            player.imgRun = player.imgAnimation.walk.imge;
+            player.imgIndexEnd = player.imgAnimation.walk.indexNumber;
+
             break;
         case "Space":
             if (player.positer.y >= 200) {
-                player.velocity.y = -player.width;
+                player.velocity.y = -10;
+                // player.imgRun=player.imgAnimation.jump.imge;
+                // player.imgIndexEnd=player.imgAnimation.jump.indexNumber;
             }
             break;
         case "KeyE":
             keyEvt.dame.visit = true;
             playerHits = 1;
+            player.imgAnimation.idle.isThis = false;
+            if(player.imgAnimation.idle.isThis==false){
+                player.imgRun = player.imgAnimation.attack.imge;
+                player.imgIndexEnd = player.imgAnimation.attack.indexNumber;
+            }
             break;
         default:
             break;
@@ -271,13 +326,32 @@ window.addEventListener("keyup", (evt) => {
     switch (evt.code) {
         case "KeyD":
             keyEvt.right.visit = false;
+            if (player.imgAnimation.idle.isThis == true) {
+
+                player.imgRun = player.imgAnimation.idle.imge;
+                player.imgIndexEnd = player.imgAnimation.idle.indexNumber;
+            }
+
             break;
         case "KeyA":
             keyEvt.left.visit = false;
+            if (player.imgAnimation.idle.isThis == true) {
+
+                player.imgRun = player.imgAnimation.idle.imge;
+                player.imgIndexEnd = player.imgAnimation.idle.indexNumber;
+            }
+
             break;
         case "KeyE":
             keyEvt.dame.visit = false;
             playerHits = 0;
+      
+            setTimeout(() => {
+                player.imgRun = player.imgAnimation.idle.imge;
+                player.imgIndexEnd = player.imgAnimation.idle.indexNumber;
+                player.imgAnimation.idle.isThis = true;
+                console.log("a")
+            }, 100)
             break;
         default:
             break;
